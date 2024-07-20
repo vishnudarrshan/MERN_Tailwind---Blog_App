809867ea4ffaadd207d8013657131cf5720a8129
vishnudarrshan <vishnuorp22@gmail.com>
2024-07-20 14:25:23 +0530

    feat: Added Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
