5819522898e816b0736a61bcf5521c55c979a0bb
vishnudarrshan <vishnuorp22@gmail.com>
2024-07-24 19:50:24 +0530

    style: Styled signup page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,55 @@
+import { Button, Label, TextInput } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link to={"/"} className=" text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              VD's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5 ">
+            Welcome to <span className="font-bold">VD'S</span> Blog.You can
+            signup with your email and password or with Google
+          </p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="text"
+                placeholder="name@company.com"
+                id="email"
+              />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button gradientDuoTone={"purpleToPink"} type="submit">
+              Sign up
+            </Button>
+          </form>
+          <div className="flex gap-2 mt-5 text-sm ">
+            <span>Have an account?</span>
+            <Link to={"/sign-in"} className="text-sky-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
