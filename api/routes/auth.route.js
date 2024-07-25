97f3808a5a7ca0fd5ab14bd7a60556fe7599e40a
vishnudarrshan <vishnuorp22@gmail.com>
2024-07-25 15:51:51 +0530

    create: Created signin api

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import e from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 
 const router = e.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
