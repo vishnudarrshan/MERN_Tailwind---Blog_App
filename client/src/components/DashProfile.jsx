39e5754a23f2bcda7a79af46eafbffa1f542288a
vishnudarrshan <vishnuorp22@gmail.com>
2024-08-09 20:19:29 +0530

    feat: Added edit profile in dashboard

---
+++ client/src/components/DashProfile.jsx
@@ -0,0 +1,42 @@
+import { Button, TextInput } from "flowbite-react";
+import { useSelector } from "react-redux";
+
+const DashProfile = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 self-center cursor-pointer overflow-hidden rounded-full shadow-md">
+          <img
+            src={currentUser.profilePicture}
+            alt="user"
+            className="rounded-full w-full h-full border-8 object-cover border-[lightgray]"
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type="text"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+        <TextInput type="text" id="password" placeholder="password" />
+        <Button type="submit" gradientDuoTone={"purpleToBlue"} outline>
+          Update
+        </Button>
+      </form>
+      <div className="text-red-500 flex justify-between mt-5">
+        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
+};
+
+export default DashProfile;
---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,47 @@
+import { Sidebar } from "flowbite-react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const DashSidebar = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, []);
+  return (
+    <Sidebar className="w-full dark:border-none border-r-2 shadow-md">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to={"/dashboard/?tab=profile"}>
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Link to={"/dashboard/?tab=signout"}>
+            {" "}
+            <Sidebar.Item
+              active={tab === "signout"}
+              icon={HiArrowSmRight}
+              label={"User"}
+              labelColor="dark"
+            >
+              Sign Out
+            </Sidebar.Item>
+          </Link>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
+export default DashSidebar;
